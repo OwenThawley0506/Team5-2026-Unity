ad282796bf378fd8fb3d008950cf41a11852ea2d
OwenThawley0506 <c4015375@my.shu.ac.uk>
2026-02-17 20:58:28 +0000

    Done UI, Open/Close Button, Pause

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public GameObject InventoryMenu;
+    private bool MenuActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Inventory") && MenuActive)
+        {
+            Time.timeScale = 1;
+            InventoryMenu.SetActive(false);
+            MenuActive = false;
+        }
+
+        else if (Input.GetButtonDown("Inventory") && !MenuActive)
+        {
+            Time.timeScale = 0;
+            InventoryMenu.SetActive(true);
+            MenuActive = true;
+        }
+    }
+}
