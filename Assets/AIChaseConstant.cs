001168b9c067cd52936f55e4db270081b4d7c7ed
OwenThawley0506 <c4015375@my.shu.ac.uk>
2026-02-24 19:45:23 +0000

    Enemies done

---
+++ Assets/AIChaseConstant.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIChaseConstant : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+}
