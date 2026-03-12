2554dba26bb693ec076300ec9af178388289dd0d
darksoul2626 <drewlove765@gmail.com>
2026-03-12 17:59:49 +0000

    Testing dialogue

--- Assets/Ink dialog scripts/OtherDudes D.ink
+++ Assets/Ink dialog scripts/OtherDudes D.ink
@@ -1,12 +1,10 @@
 -> first
 
 === first ===
-a dude walked up to a lemonade stand and said to the man running the stand
-MC: heyyyy got any grapes
+MC: heyyyy got any grapes #Speaker:MainCharacter #Portrait:Normal #Layout:Left
 
-Stranger: no, we just sell lemonade but its cold and its fresh and its all homemade, can i get you a glass?
+Stranger: no, we just sell lemonade but its cold and its fresh and its all homemade, can i get you a glass? #Speaker:Strangertranger #Portrait:normal #Layout:Right
 
-MC: ill pass
+MC: ill pass #Speaker:Maincharacter #Portrait:Sad #Layout:Left
 
-then he walded away.
 -> END
--- Assets/Scripts/Dialogue/Dialog Manager.cs
+++ Assets/Scripts/Dialogue/Dialog Manager.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Ink.Runtime;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SearchService;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -13,8 +15,11 @@ public class DialogueManager : MonoBehaviour
     private Story currentStory;
     public bool dialogueIsPlaying { get; private set; }
 
-    private static DialogueManager instance;  
-
+    private static DialogueManager instance;
+    private object splitTag;
+    private const string Speaker_Tag = "Speaker";
+    private const string Portrait_Tag = "Portrait";
+    private const string Layout_Tag = "Layout";
     private void Awake()
     {
         instance = this;
@@ -76,10 +81,47 @@ public class DialogueManager : MonoBehaviour
         if (currentStory.canContinue)
         {
             dialogueText.text = currentStory.Continue();
+            HandleTags(currentStory.currentTags);
         }
         else
         {
             StartCoroutine(ExitDialogueMode());
         }
+     }
+     private void HandleTags(List<string> currentTags)
+     {
+        //loop through each tag and handle it accordingly
+        foreach (string tag in currentTags)
+        {
+            //split the tag into its name and value (if it has a value)
+            string[] splitTag = tag.Split(":");
+            if (splitTag.Length != 2)
+            }
+            Debug.LogError("Tag could not be appropriatly split: " + tag);
+            {
+
+        string tagKey = splitTag[0].Trim();
+        string tagValue = splitTag[1].Trim();
+
+            //Handle the tag
+            switch (tagKey)
+            {
+                //handle speaker tag
+                case Speaker_Tag:
+                    Debug.Log("speaker tag value: " + tagValue);
+                    break;
+                    //handle portrait tag
+                case Portrait_Tag:
+                    Debug.Log("portrait tag value: " + tagValue);
+                    break;
+                    //handle layout tag
+                case Layout_Tag:
+                    Debug.Log("layout tag value: " + tagValue);
+                    break;
+                default:
+                    Debug.LogWarning("Tag came in but is currently not beinghandled: " + tag);
+                    break;
+            }
+        } 
      }
 }
